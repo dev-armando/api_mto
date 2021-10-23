372a20215121420652a4bae8fb16a66e9654abf1
= <=>
2021-10-23 11:23:39 -0400

    added usuariosbanda migration

---
+++ database/migrations/2021_10_23_031926_updated_usuariosbandas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatedUsuariosbandasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('usuariosbandas', function (Blueprint $table) {
+            $table->string('refresh_token')->nullable();
+            $table->string('access_token')->nullable();
+            $table->date("time_token")->nullable();
+            $table->string("fee")->nullable();
+            $table->string("number_random")->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('usuariosbandas', function (Blueprint $table) {
+            $table->dropColumn(['refresh_token' , 'access_token' , 'time_token' , 'fee' , 'number_random' ]);
+        });
+    }
+}
