1e0da00c2befdf7d7401d5a476c578cc82267fd7
= <=>
2021-10-30 11:03:21 -0400

    updated

--- public/index.php
+++ public/index.php
@@ -1,7 +1,31 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 ini_set('session.cookie_domain', '.meticketonline.com');
-session_start();
 
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+/*if(!isset($_SESSION['idusuario'])) {
+    print(json_encode( array( 'error' => 403 ) ) ); exit;
+}*/
+
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: *");
+   // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
 var_dump($_SESSION); exit;
 
 /**
